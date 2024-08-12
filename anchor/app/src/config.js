1b3728e737c2c40a6dccdbebf1b243304b5ba998
Virjilakrum <asesenep15@gmail.com>
2024-08-12 14:53:21 +0300

    Update config.js

--- anchor/app/src/config.js
+++ anchor/app/src/config.js
@@ -1,4 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const SOLANA_HOST = process.env.REACT_APP_SOLANA_RPC_HOST;
-export const PROGRAM_ID = new PublicKey(process.env.REACT_APP_PROGRAM_ID);
+export const SOLANA_HOST =
+  process.env.REACT_APP_SOLANA_RPC_HOST || "https://api.devnet.solana.com";
+export const PROGRAM_ID = new PublicKey(
+  process.env.REACT_APP_PROGRAM_ID ||
+    "8BThYttJrWRaQYwZmYkZiHQrE5nLaGYCS5mWXapyEEoa"
+);
