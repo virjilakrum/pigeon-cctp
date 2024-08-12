2d5c3f64c2712818115b1976bfc85675928a2238
Virjilakrum <asesenep15@gmail.com>
2024-08-12 21:46:07 +0300

    setup create_transfer

--- anchor/programs/pigeon-cctp/src/lib.rs
+++ anchor/programs/pigeon-cctp/src/lib.rs
@@ -10,19 +10,28 @@ pub mod pigeon_cctp {
         let transfer_data = &mut ctx.accounts.transfer_data;
         transfer_data.amount = 0;
         transfer_data.from = *ctx.accounts.user.key;
+        transfer_data.to = Pubkey::default();
+        transfer_data.destination_chain = String::new();
         Ok(())
     }
 
-    pub fn create_transfer(ctx: Context<CreateTransfer>, amount: u64) -> Result<()> {
+    pub fn create_transfer(
+        ctx: Context<CreateTransfer>,
+        amount: u64,
+        to: Pubkey,
+        destination_chain: String,
+    ) -> Result<()> {
         let transfer_data = &mut ctx.accounts.transfer_data;
         transfer_data.amount = amount;
+        transfer_data.to = to;
+        transfer_data.destination_chain = destination_chain;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8 + 32)]
+    #[account(init, payer = user, space = 8 + 8 + 32 + 32 + 64)]
     pub transfer_data: Account<'info, TransferData>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -40,4 +49,6 @@ pub struct CreateTransfer<'info> {
 pub struct TransferData {
     pub amount: u64,
     pub from: Pubkey,
+    pub to: Pubkey,
+    pub destination_chain: String,
 }
